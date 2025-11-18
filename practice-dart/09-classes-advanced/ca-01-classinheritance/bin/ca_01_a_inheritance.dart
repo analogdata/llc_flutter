90119595ee7ba7b7e93d02be34982eb6ebf687e9
analogdata <rajath@analogdata.io>
2025-11-18 11:09:23 +0530

    Animal Example

---
+++ practice-dart/09-classes-advanced/ca-01-classinheritance/bin/ca_01_a_inheritance.dart
@@ -0,0 +1,50 @@
+class Animal {
+  Animal({required this.age});
+
+  final int age;
+
+  void sleep() => print("Sleep");
+  void eat() => print("Eat");
+}
+
+class Lion extends Animal {
+  Lion({required super.age});
+  void hunt() => print("Hunt");
+}
+
+class Dog extends Animal {
+  Dog({required super.age});
+  void bark() => print("Bark");
+}
+
+class Tiger extends Animal {
+  Tiger({required super.age});
+  void roar() => print("Roar");
+}
+
+class Tortoise extends Animal {
+  Tortoise({required super.age});
+  void swim() => print("Swimming...");
+}
+
+void main() {
+  final puppy = Dog(age: 2);
+  puppy.sleep();
+  puppy.bark();
+  print(puppy.age);
+
+  final tiger = Tiger(age: 5);
+  tiger.sleep();
+  tiger.roar();
+  print(tiger.age);
+
+  final tortoise = Tortoise(age: 105);
+  tortoise.sleep();
+  tortoise.swim();
+  print(tortoise.age);
+
+  final simba = Lion(age: 3);
+  simba.sleep();
+  simba.hunt();
+  print(simba.age);
+}
